c3ddfc73b8580f1101b5b6092905df731bf30666
c1983 <896850945@qq.com>
2024-05-25 17:13:33 +0800

    可以了

--- 基础/判断奇数.cpp
+++ 基础/判断奇数.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int a;
-	scanf("%d\n", &a);
+	int a=0;
+	scanf_s("%d", &a);
+	if (a % 2 == 0)
+	{
+		printf("不是奇数");
+	}
+	else
+	{
+		printf("是奇数");
+	}
 	return 0;
 }
