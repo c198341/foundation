3d0d6723e74295a3db5d26304a946214440ac1a4
c1983 <896850945@qq.com>
2024-05-26 17:25:33 +0800

    练习

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,7 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
-
+//#define MAX 100
+#define MAX(x,y)(x>y?x:y)
 #include <stdio.h>
 #include <string.h>
+//extern int add(int, int);
+////int cp(int n1, int n2)
+////{
+////
+////	if (n1 > n2)
+////
+////		return n1;
+////
+////	else
+////
+////		return n2;
+////
+////
+////}
 int main()
 {
 	///*int a=0;
@@ -29,13 +44,49 @@ int main()
 	//printf("%d\n", me1);
 	//printf("%d\n", strlen("c:\test\628\test.c"));
 	//printf("%c\n",'\63\63');*/
-	int line = 0;
-	printf("加入比特\n");
-	while (line <= 10)
-	{
-		printf("继续敲代码:%d\n",line);
-			line++;
-	}
-	printf("good offer");
+	///*int line = 0;
+	//printf("加入比特\n");
+	//while (line <= 10)
+	//{
+	//	printf("继续敲代码:%d\n",line);
+	//		line++;
+	//}
+	//printf("good offer");*/
+	//int i = 0;
+	//float arr[2] = { 1.000001,2};
+	//for (i = 0; i < 2; i++)
+	//{
+	//	printf("%f ", arr[i]);
+	//}
+	////arr[2] =2.1;
+ ////   /*i = 0;
+	////printf("\n");
+	////for (i = 0;i < 10; i++)
+	////{
+	////	printf("%p ", arr[i]);
+	////}*/
+	///*int a = 3;
+	//int b = 5;
+	//int c = a ^ b;
+	//printf("%d\n", c);*/
+	
+	///*int a;
+	//int b;
+	//int c;
+	//scanf("%d %d", &a, &b);
+	//c = cp(a,b);
+	//printf("最大值是：%d", c);
+	///*register int i = 0;
+	//int arr[10] = { 0 };
+	//for (i = 0; i <= 9; i++)
+	//
+	//	printf("%d\n", arr[i]);*/
+	///*extern int g;*/
+	//int a = 100;
+	//int b = 220;
+	//int sum = MAX(a, b);
+	//printf("%d\n", sum);
+	////printf("%d\n", MAX * 0.555555);
+
 	return 0;
 }
---
+++ Project1/源1.cpp
@@ -0,0 +1,7 @@
+//static int g = 2024;
+//static 
+int add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
