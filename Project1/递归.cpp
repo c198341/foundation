3a1332515d9ec60ed13bea5ad9feacd5961dd259
c1983 <896850945@qq.com>
2024-06-08 07:44:20 +0800

    1

--- Project1/递归.cpp
+++ Project1/递归.cpp
@@ -4,19 +4,21 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <time.h>
-int jiecheng(int x)
-{
-	while (x > 1)
-	{
-		return x*jiecheng(x - 1);
-	}
-	return 1;
-}
+//int jiecheng(int x)
+//{
+//	while (x > 1)
+//	{
+//		return x*jiecheng(x - 1);
+//	}
+//	return 1;
+//}
 int main()
 {
-	int i=1;
-	int j;
-	j = jiecheng(i);
-	printf("%d\n", j);
+	//int i=5;
+	//int j;
+	//j = jiecheng(i);
+	//printf("%d\n", j);
+	int i = 5;
+	int a[6] = {0};
 	return 0;
 }
