0ff5dbbfe2ed162065e72859021f2a4372609971
c1983 <896850945@qq.com>
2024-06-21 21:49:37 +0800

    1

--- Project1/练习.c
+++ Project1/练习.c
@@ -1,5 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+struct stu
+{
+	char name[20];
+	int age;
+	char id[10];
+};
 int main()
 {
 	///*int num=-1;
@@ -13,17 +19,27 @@ int main()
 	//	num = num / 2;
 	//}
 	//printf("%d\n", count);*/
-	int num =15;
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < 32; i++)
-	{
-		if (1 == (num & 1) % 2)
-		{
-			j++;
-		}
-		num = num >> 1;
-	}
-	printf("%d", j);
+	///*int num =15;
+	//int i = 0;
+	//int j = 0;
+	//for (i = 0; i < 32; i++)
+	//{
+	//	if (1 == (num & 1) % 2)
+	//	{
+	//		j++;
+	//	}
+	//	num = num >> 1;
+	//}
+	//printf("%d", j);*/
+
+	struct stu s1 = { "zhang",20,"20240110" };
+	struct stu* ps = &s1;
+	printf("%s\n", s1.name);
+	printf("%d\n", s1.age);
+	printf("%s\n", s1.id);
+	printf("%s\n", (*ps).name);
+	printf("%d\n", (*ps).age);
+	printf("%s\n", (*ps).id);
+	printf("%s\n", ps->name);
 	return 0;
 }
