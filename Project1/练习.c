819f17e24c0105059ae25b126b497e970c6968f9
c1983 <896850945@qq.com>
2024-08-09 21:23:05 +0800

    7

--- Project1/练习.c
+++ Project1/练习.c
@@ -987,12 +987,17 @@ int main()
 	int i, j;
 	int a[10][10];
 	a[0][0] = 1;
-	printf("%d", a[0][0]);
-	for(i=0;i<10;i++)
-		for (j = 0; j <= i + 1; j++)
+	printf("%d\n", a[0][0]);
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j <= i + 1; j++)
 		{
-			if(j==0)
-				a[][]
+			if (j == 0)
+				a[i][j] = 1;
+			a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+			printf(" %d ", a[i][j]);
 		}
+		printf("\n");
+	}	
 	return 0;
 }
