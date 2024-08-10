0c49e8915c3c29526a9f8ff7a0332dfe8f059019
c1983 <896850945@qq.com>
2024-08-10 21:25:10 +0800

    5

--- Project1/练习.c
+++ Project1/练习.c
@@ -982,23 +982,37 @@
 //	printf("\n");
 //	return 0;
 //}
+//int main()
+//{
+//	int i, j;
+//	int a[10][10];
+//	for (i = 0; i < 10; i++)
+//	{
+//		a[i][0] = 1;
+//		a[i][i] = 1;
+//	}
+//	for (i = 2; i < 10; i++)
+//	{
+//		for (j = 1; j <= i - 1; j++)
+//		{
+//			a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+//		}
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%5d", a[i][j]);
+//		}
+//		printf("\n");
+//	}		
+//	return 0;
+//}
 int main()
 {
-	int i, j;
-	int a[10][10];
-	a[0][0] = 1;
-	printf("%d\n", a[0][0]);
-	for (i = 1; i < 10; i++)
-	{
-		for (j = 1; j <= i + 1; j++)
-		{
-			if ((j+1) == i)
-				a[i][j] = 1;
-			else
-				a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
-			printf(" %d ", a[i][j]);
-		}
-		printf("\n");
-	}	
+	int n;
+	scanf("%d", &n);
+	printf("1-n*nµÄÄ§·½Õó:\n");
+
 	return 0;
 }
