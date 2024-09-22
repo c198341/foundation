437604babf4f6c71570cc75b49900a6fa8e4bc0f
c1983 <896850945@qq.com>
2024-09-22 15:41:30 +0800

    4

--- 赛程过半/赛程过半.cpp
+++ 赛程过半/赛程过半.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 #include <math.h>
+#include <stdlib.h>
 //void rarr(char* arr, int sz)
 //{
 //	assert(arr);
@@ -322,45 +323,59 @@
 //		printf("not find\n");
 //	return 0;
 //}
-int int_cmp(const void* p1, const void* p2)
-{
-	return (*(int*)p1 - *(int*)p2);
-}
-void _swap(void* p1, void* p2, int size)
-{
-	int i = 0;
-	for (i = 0; i < size; i++)
-	{
-		char tmp = *((char*)p1 + i);
-		*((char*)p1 + i) = *((char*)p2 + i);
-		*((char*)p2 + i) = tmp;
-	}
-}
-void bubble(void* base, int count, int size, int(*cmp)(void*, void*))
-{
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < count - 1; i++)
-	{
-		for (j = 0; j < count - i - 1; j++)
-		{
-			if (cmp((char*)base + j * size, (char*)base + (j + 1) * size) > 0)
-			{
-				_swap((char*)base + j * size, (char*)base + (j + 1) * size, size);
-			}
-		}
-	}
-}
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//};
+//int sort_by_name(const void* p1, const void* p2)
+//{
+//	return strcmp(((struct stu*)p1)->name, ((struct stu*)p2)->name);
+//}
+//int sort_by_age(const void* p1, const void* p2)
+//{
+//	return (((struct stu*)p1)->age-((struct stu*)p2)->age);
+//}
+////void _swap(void* p1, void* p2, int size)
+//{
+//	int i = 0;
+//	for (i = 0; i < size; i++)
+//	{
+//		char tmp = *((char*)p1 + i);
+//		*((char*)p1 + i) = *((char*)p2 + i);
+//		*((char*)p2 + i) = tmp;
+//	}
+//}
+//void bubble(void* base, int count, int size, int(*sort_by_age)(const void*,const void*))
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < count - 1; i++)
+//	{
+//		for (j = 0; j < count - i - 1; j++)
+//		{
+//			if (sort_by_name((char*)base + j * size, (char*)base + (j + 1) * size) > 0)
+//			{
+//				_swap((char*)base + j * size, (char*)base + (j + 1) * size, size);
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	//int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
+//	//char *arr[] = {"aaaa","dddd","cccc","bbbb"};
+//	struct stu s[3] = { {"z",20},{"l",30},{"w",10} };
+//	int i = 0;
+//	qsort(s, sizeof(s) / sizeof(s[0]), sizeof(s[0]), sort_by_age);
+//	///*for (i = 0; i < sizeof(s) / sizeof(arr[0]); i++)
+//	//{
+//	//	printf("%d ", arr[i]);
+//	//}*/
+//	printf("\n");
+//	return 0;
+//}
 int main()
 {
-	int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
-	//char *arr[] = {"aaaa","dddd","cccc","bbbb"};
-	int i = 0;
-	bubble(arr, sizeof(arr) / sizeof(arr[0]), sizeof(int), int_cmp);
-	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-	return 0;
+
 }
