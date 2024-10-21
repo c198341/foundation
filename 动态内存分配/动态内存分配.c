a04c88379d8e708ad21e633367932116860868e3
c1983 <896850945@qq.com>
2024-10-22 07:28:06 +0800

    8

--- 动态内存分配/动态内存分配.c
+++ 动态内存分配/动态内存分配.c
@@ -3,12 +3,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+//int main()
+//{
+//	int* p = (int*)malloc(10000 * sizeof(int));
+//	if (p == NULL)
+//		printf("%s\n", strerror(errno));
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+char* GetMemory(char* p)
+{
+	p = (char*)malloc(100);
+	return p;
+}
+void Test(void)
+{
+	char* str = NULL;
+	str=GetMemory(str);
+	strcpy(str, "hello world");
+	printf(str);
+	free(str);
+	str = NULL;
+}
 int main()
 {
-	int* p = (int*)malloc(10000 * sizeof(int));
-	if (p == NULL)
-		printf("%s\n", strerror(errno));
-	free(p);
-	p = NULL;
+	Test();
 	return 0;
 }
