ab05cba372fb83bb4b0456b3c7b2777719e4233b
c1983 <896850945@qq.com>
2024-10-22 21:26:09 +0800

    7

--- 动态内存分配/动态内存分配.c
+++ 动态内存分配/动态内存分配.c
@@ -31,13 +31,43 @@
 //	Test();
 //	return 0;
 //}
-struct stu
+//struct stu
+//{
+//	int n;
+//	int arr[0];
+//};
+//int main()
+//{
+//	struct stu s = {1,0};
+//	return 0;
+//}
+//char* new(int n)
+//{
+//	return malloc(n);
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	char* p= new(n);
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+void bin(int value)
 {
-	int n;
-	int arr[0];
-};
+
+}
+void getbits(int value, int n1, int n2)
+{
+
+}
 int main()
 {
-	struct stu s = {1,0};
+	int value,n1,n2;
+	scanf("%o", &value);
+	bin(value);
+	scanf("%d %d", &n1, &n2);
+	getbits(value, n1, n2);
 	return 0;
 }
