3fb6fc8e6df2a7e14e5417daadad3fbecb2a1934
c1983 <896850945@qq.com>
2024-10-22 08:06:42 +0800

    4

--- 动态内存分配/动态内存分配.c
+++ 动态内存分配/动态内存分配.c
@@ -12,22 +12,32 @@
 //	p = NULL;
 //	return 0;
 //}
-char* GetMemory(char* p)
+//char* GetMemory(char* p)
+//{
+//	p = (char*)malloc(100);
+//	return p;
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	str=GetMemory(str);
+//	strcpy(str, "hello world");
+//	printf(str);
+//	free(str);
+//	str = NULL;
+//}
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+struct stu
 {
-	p = (char*)malloc(100);
-	return p;
-}
-void Test(void)
-{
-	char* str = NULL;
-	str=GetMemory(str);
-	strcpy(str, "hello world");
-	printf(str);
-	free(str);
-	str = NULL;
-}
+	int n;
+	int arr[0];
+};
 int main()
 {
-	Test();
+	struct stu s = {1,0};
 	return 0;
 }
