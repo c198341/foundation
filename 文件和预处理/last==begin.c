bd3de364601d757348a5ee63da4f67fbb49a975c
c1983 <896850945@qq.com>
2024-11-03 14:31:47 +0800

    8

--- 文件和预处理/last==begin.c
+++ 文件和预处理/last==begin.c
@@ -33,38 +33,76 @@
 //    pf = NULL;
 //    return 0;
 //}
+//int main()
+//{
+//    char arra[10] = {0};
+//    char arrb[10] = {0};
+//    char tmp;
+//    int i, j;
+//    FILE* pa = fopen("a.txt", "w");
+//    if (pa == NULL)
+//    {
+//        printf("%s\n", strerror(errno));
+//        return 0;
+//    }
+//    scanf("%s", arra);
+//    fputs(arra, stdout);
+//    printf("\n");
+//    fwrite(arra, sizeof(arra), 1, pa);
+//    FILE* pb = fopen("b.txt", "w");
+//    if (pb == NULL)
+//    {
+//        printf("%s\n", strerror(errno));
+//        return 0;
+//    }
+//    scanf("%s", arrb);
+//    fwrite(arrb, sizeof(arrb), 1, pb);
+//    fputs(arrb, stdout);
+//    printf("\n");
+//    strcat(arra, arrb);
+//    for(i=0;i<strlen(arra)-1;i++)
+//        for (j = i + 1; j < strlen(arra); j++)
+//        {
+//            if (arra[i] > arra[j])
+//            {
+//                tmp = arra[i];
+//                arra[i] = arra[j];
+//                arra[j] = tmp;
+//            }
+//        }
+//    FILE* pc = fopen("c.txt", "w");
+//    pc = fopen("c.txt", "w");
+//    fwrite(arra, sizeof(arra), 1, pc);
+//    fputs(arra, stdout);
+//    printf("\n");
+//    fclose(pa);
+//    pa = NULL;
+//    fclose(pb);
+//    pb = NULL;
+//    fclose(pc);
+//    pc = NULL;
+//    return 0;
+//}
+typedef struct score
+{
+	char num[4];
+	char name[10];
+	double chinese;
+	double math;
+}score;
+typedef struct stu
+{
+	int n;
+	score* sco;
+}stu;
 int main()
 {
-    char arr[10];
-    FILE* pa = fopen("a.txt", "w");
-    if (pa == NULL)
-    {
-        printf("%s\n", strerror(errno));
-        return 0;
-    }
-    scanf("%s", arr);
-    fwrite(arr, sizeof(arr), 1, pa);
-    FILE* pb = fopen("b.txt", "w");
-    if (pb == NULL)
-    {
-        printf("%s\n", strerror(errno));
-        return 0;
-    }
-    scanf("%s", arr);
-    fwrite(arr, sizeof(arr), 1, pb);
-    FILE* pc = fopen("c.txt", "w");
-    //fputs(pa,pc);
-    fwrite(pa, sizeof(arr), 1, pc);
-    fclose(pc);
-    pc = NULL;
-    pc = fopen("c.txt", "a");
-    //fputs(pb,pc);
-    fwrite(pb, sizeof(arr), 1, pc);
-    fclose(pa);
-    pa = NULL;
-    fclose(pb);
-    pb = NULL;
-    fclose(pc);
-    pc = NULL;
-    return 0;
+	int i;
+	stu* p;
+	score* scores[5]=(score*)malloc(5*sizeof(score));
+	for(p->n=0;p->n<4;p-n++)
+	{
+		scanf("%s %s %.1f %.1f", scores[p->n]);
+	}
+	return 0;
 }
