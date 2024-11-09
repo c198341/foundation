85700e8185ec57fe16fd8e5c65648a1f197172db
c1983 <896850945@qq.com>
2024-11-09 12:44:09 +0800

    4

--- 文件和预处理/last==begin.c
+++ 文件和预处理/last==begin.c
@@ -83,19 +83,19 @@
 //    pc = NULL;
 //    return 0;
 //}
-typedef struct score
-{
-	char num[10];
-	char name[10];
-	double chinese;
-	double math;
-	double ave;
-}score;
-typedef struct stu
-{
-	int n;
-	score* sco;
-}stu;
+//typedef struct score
+//{
+//	char num[10];
+//	char name[10];
+//	double chinese;
+//	double math;
+//	double ave;
+//}score;
+//typedef struct stu
+//{
+//	int n;
+//	score* sco;
+//}stu;
 //void my_print()
 //{
 //	FILE* pf = fopen("stud.txt", "r");
@@ -150,99 +150,118 @@ typedef struct stu
 //	fclose(pf2);
 //	pf2 = NULL;
 //}
+//int main()
+//{
+//	///*stu con;
+//	//stu* pstu= &con;
+//	//score* ptr = (score*)malloc(5*sizeof(score));
+//	//if (ptr == NULL)
+//	//{
+//	//	printf("%s\n", strerror(errno));
+//	//	return 0;
+//	//}
+//	//pstu->sco = ptr;
+//	//for(pstu->n=0;pstu->n<3;pstu->n++)
+//	//{
+//	//	scanf("%s %s %lf %lf", pstu->sco[pstu->n].num,
+//	//		                     pstu->sco[pstu->n].name,
+//	//		                     &(pstu->sco[pstu->n].chinese),
+//	//		                    &(pstu->sco[pstu->n].math));
+//	//}
+//	//int i;
+//	//double stu_ave[3];
+//	//for (i = 0; i < pstu->n; i++)
+//	//{
+//	//	stu_ave[i] = ((pstu->sco[i].chinese) + (pstu->sco[i].math)) / 2;
+//	//	pstu->sco[i].ave = stu_ave[i];
+//	//}
+//	//FILE* pf = fopen("stud.txt", "w");
+//	//if (pf == NULL)
+//	//{
+//	//	printf("%s\n", strerror(errno));
+//	//	return 0;
+//	//}
+//	//for (i = 0; i < pstu->n; i++)
+//	//{
+//	//	fwrite(&(pstu->sco[i]), sizeof(score), 1, pf);
+//	//}
+//	//fclose(pf);
+//	//pf = NULL;
+//	//free(pstu->sco);
+//	//pstu->sco = NULL;
+//	//my_print();
+//	//my_sort();
+//	//return 0;*/
+//	score s[4];
+//	FILE* pf = fopen("D:\\驱动精灵搬家目录\\foundation\\文件和预处理\\stud_sort.txt", "r");
+//	if (pf == NULL)
+//	{
+//		printf("pf=%s\n", strerror(errno));
+//		return 0;
+//	}
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		fread(&(s[i]), sizeof(struct score), 1, pf);
+//	}
+//	scanf("%s %s %lf %lf",s[3].num,s[3].name,&(s[3].chinese),&(s[3].math));
+//	s[3].ave=( (s[3].chinese)+(s[3].math))/2;
+//	score ss;
+//	for (i = 3; i >= 1; i--)
+//	{
+//		if (s[i-1].ave < s[i].ave)
+//		{
+//			ss = s[i];
+//			s[i]=s[i-1];
+//			s[i-1]=ss;
+//		}
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	FILE* pf1 = fopen("stud_sort.txt", "w");
+//	if (pf1 == NULL)
+//	{
+//		printf("pf1=%s\n", strerror(errno));
+//		return 0;
+//	}
+//	for (i = 0; i < 4; i++)
+//	{
+//		fwrite(&s[i], sizeof(score), 1, pf1);
+//	}
+//	fclose(pf1);
+//	pf1 = NULL;
+//	FILE* pf2 = fopen("stud_sort.txt", "r");
+//	if (pf2 == NULL)
+//	{
+//		printf("pf2=%s\n", strerror(errno));
+//		return 0;
+//	}
+//	for (i = 0; i < 4; i++)
+//	{
+//		fread(&s[i], sizeof(score), 1, pf2);
+//		printf("%s %s %lf %lf %lf\n", s[i].num, s[i].name, s[i].chinese, s[i].math,
+//			s[i].ave);
+//	}
+//	fclose(pf2);
+//	pf2 = NULL;
+//	return 0;
+//}
 int main()
 {
-	///*stu con;
-	//stu* pstu= &con;
-	//score* ptr = (score*)malloc(5*sizeof(score));
-	//if (ptr == NULL)
-	//{
-	//	printf("%s\n", strerror(errno));
-	//	return 0;
-	//}
-	//pstu->sco = ptr;
-	//for(pstu->n=0;pstu->n<3;pstu->n++)
-	//{
-	//	scanf("%s %s %lf %lf", pstu->sco[pstu->n].num,
-	//		                     pstu->sco[pstu->n].name,
-	//		                     &(pstu->sco[pstu->n].chinese),
-	//		                    &(pstu->sco[pstu->n].math));
-	//}
-	//int i;
-	//double stu_ave[3];
-	//for (i = 0; i < pstu->n; i++)
-	//{
-	//	stu_ave[i] = ((pstu->sco[i].chinese) + (pstu->sco[i].math)) / 2;
-	//	pstu->sco[i].ave = stu_ave[i];
-	//}
-	//FILE* pf = fopen("stud.txt", "w");
-	//if (pf == NULL)
-	//{
-	//	printf("%s\n", strerror(errno));
-	//	return 0;
-	//}
-	//for (i = 0; i < pstu->n; i++)
-	//{
-	//	fwrite(&(pstu->sco[i]), sizeof(score), 1, pf);
-	//}
-	//fclose(pf);
-	//pf = NULL;
-	//free(pstu->sco);
-	//pstu->sco = NULL;
-	//my_print();
-	//my_sort();
-	//return 0;*/
-	score s[4];
-	FILE* pf = fopen("D:\\驱动精灵搬家目录\\foundation\\文件和预处理\\stud_sort.txt", "r");
+	FILE* pf = fopen("t.txt", "r");
 	if (pf == NULL)
 	{
-		printf("pf=%s\n", strerror(errno));
 		return 0;
 	}
-	int i = 0;
-	for (i = 0; i < 3; i++)
-	{
-		fread(&(s[i]), sizeof(struct score), 1, pf);
-	}
-	scanf("%s %s %lf %lf",s[3].num,s[3].name,&(s[3].chinese),&(s[3].math));
-	s[3].ave=( (s[3].chinese)+(s[3].math))/2;
-	score ss;
-	for (i = 3; i >= 1; i--)
-	{
-		if (s[i-1].ave < s[i].ave)
-		{
-			ss = s[i];
-			s[i]=s[i-1];
-			s[i-1]=ss;
-		}
-	}
+	fseek(pf, -3, SEEK_END);
+	char ch = fgetc(pf);
+	printf("%c\n", ch);
+	int i = ftell(pf);
+	printf("%d\n", i);
+	rewind(pf);
+	ch = fgetc(pf);
+	printf("%c\n", ch);
 	fclose(pf);
 	pf = NULL;
-	FILE* pf1 = fopen("stud_sort.txt", "w");
-	if (pf1 == NULL)
-	{
-		printf("pf1=%s\n", strerror(errno));
-		return 0;
-	}
-	for (i = 0; i < 4; i++)
-	{
-		fwrite(&s[i], sizeof(score), 1, pf1);
-	}
-	fclose(pf1);
-	pf1 = NULL;
-	FILE* pf2 = fopen("stud_sort.txt", "r");
-	if (pf2 == NULL)
-	{
-		printf("pf2=%s\n", strerror(errno));
-		return 0;
-	}
-	for (i = 0; i < 4; i++)
-	{
-		fread(&s[i], sizeof(score), 1, pf2);
-		printf("%s %s %lf %lf %lf\n", s[i].num, s[i].name, s[i].chinese, s[i].math,
-			s[i].ave);
-	}
-	fclose(pf2);
-	pf2 = NULL;
 	return 0;
 }
