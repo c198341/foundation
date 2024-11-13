cf6912fc5c2d4d486d23dd74ad548d9877e02b48
c1983 <896850945@qq.com>
2024-11-13 21:38:01 +0800

    8

--- 文件和预处理/last==begin.c
+++ 文件和预处理/last==begin.c
@@ -286,320 +286,363 @@
 //	pf = NULL;
 //	return 0;
 //}
-#define CAPABILITY 2
-typedef struct employ
-{
-	char name[10];
-	int age;
-	double salary;
-}emp;
-typedef struct employ_sim
-{
-	char name[10];
-	double salary;
-}emp_sim;
-typedef struct emp_info
-{
-	emp* e;
-	int emp_pre;
-	int emp_capability;
-}emp_info;
-typedef struct emp_sim_info
-{
-	emp_sim* ee;
-	int emp_sim_pre;
-	int emp_sim_capability;
-}emp_sim_info;
-enum option
-{
-	EXIT,
-	ADD,
-	DELETE,
-	SAVE,
-	SHOW,
-	ADD_SIM,
-	DELETE_SIM
-};
-void checkemp(emp_info* emps)
-{
-	emp* ptr;
-	if (emps->emp_pre == emps->emp_capability)
-	{
-		ptr = (emp*)realloc(emps->e, (emps->emp_capability + 2) * sizeof(emp));
-		if (ptr == NULL)
-		{
-			return;
-		}
-		emps->e = ptr;
-		emps->emp_capability += 2;
-		printf("增容成功\n");
-	}
-}
-void loademp(emp_info* emps)
-{
-	emp tmp;
-	FILE* pf = fopen("employee.txt", "r");
-	if (pf == NULL)
-	{
-		perror("load error:");
-		return;
-	}
-	while (fread(&tmp, sizeof(emp), 1, pf))
-	{
-		checkemp(emps);
-		emps->e[emps->emp_pre] = tmp;
-		emps->emp_pre++;
-	}
-	fclose(pf);
-	pf = NULL;
-}
-void init(emp_info* emps)
-{
-	emps->e = (emp*)malloc(sizeof(emp) * 2);
-	if (emps->e == NULL)
-	{
-		return;
-	}
-	emps->emp_pre = 0;
-	emps->emp_capability = CAPABILITY;
-	loademp(emps);
-}
-void add(emp_info* emps)
-{
-	checkemp(emps);
-	printf("请输入名字:>\n");
-	scanf("%s", emps->e[emps->emp_pre].name);
-	printf("请输入年龄:>\n");
-	scanf("%d", &(emps->e[emps->emp_pre].age));
-	printf("请输入工资:>\n");
-	scanf("%lf", &(emps->e[emps->emp_pre].salary));
-	emps->emp_pre++;
-	printf("添加成功\n");
-}
-void add_sim(emp_info* emps, emp_sim_info* emps_sim)
-{
-	emp tmp;
-	emp_sim tmp_sim;
-	FILE* pf = fopen("employee.txt", "r");
-	if (pf == NULL)
-	{
-		perror("load employee error:");
-		return;
-	}
-	FILE* pf_sim = fopen("emp_sim.txt", "w");
-	if (pf_sim == NULL)
-	{
-		perror("load emp_sim error:");
-		return;
-	}
-	while (fread(&tmp, sizeof(emp), 1, pf))
-	{
-		strcpy(tmp_sim.name, tmp.name);
-		tmp_sim.salary = tmp.salary; 
-		fwrite(&tmp_sim, sizeof(emp_sim), 1, pf_sim);
-	}
-	fclose(pf);
-	pf = NULL;
-	fclose(pf_sim);
-	pf_sim = NULL;
-	printf("已建立简明职工工资文件\n");
-	FILE* pfsim = fopen("emp_sim.txt", "r");
-	int i = 0;
-	printf("%-5s\t%-5s\n", "名字", "工资");
-	for (i = 0; i < emps->emp_pre; i++)
-	{
-		fread(&tmp_sim, sizeof(emp_sim), 1, pfsim);
-		printf("%-5s\t%-5lf\n", tmp_sim.name, tmp_sim.salary);
-	}
-	fclose(pfsim);
-	pfsim = NULL;
-	emps_sim->emp_sim_pre = emps->emp_pre;
-	emps_sim->emp_sim_capability = emps->emp_capability;
-}
-int findemp(emp_info* emps,char* name)
-{
-	int i = 0;
-	for (i = 0; i < emps->emp_pre; i++)
-	{
-		if (strcmp(emps->e[i].name, name) == 0)
-		{
-			return i;
-		}
-	}
-	return -1;
-}
-void delete(emp_info* emps)
-{
-	char name[10];
-	printf("请输入要删除人员的名字：");
-	scanf("%s", name);
-	int pos = findemp(emps, name);
-	if (pos == -1)
-	{
-		printf("找不到要删除有人员\n");
-	}
-	else
-	{
-		int j = 0;
-		for (j = pos; j < emps->emp_pre - 1; j++)
-		{
-			emps->e[j] = emps->e[j + 1];
-		}
-		emps->emp_pre--;
-		printf("删除成功\n");
-	}
-}
-void delete_sim(emp_sim_info* emps_sim)
-{
-	char name[10];
-	printf("请输入要删除人员(emp_sim)的名字：");
-	scanf("%s", name);
-	emp_sim tmp;
-	FILE* pf1 = fopen("emp_sim.txt", "r");
-	if (pf1 == NULL)
-	{
-		perror("load error:");
-		return;
-	}
-	emps_sim->ee = (emp_sim*)malloc(sizeof(emp_sim) * 20);
-	int i = 0;
-	while (fread(&tmp, sizeof(emp_sim), 1, pf1))
-	{
-		emps_sim->ee[i++] = tmp;
-	}
-	fclose(pf1);
-	pf1 = NULL;
-	i = 0;
-	int pos = -1;
-	for (i = 0; i < emps_sim->emp_sim_pre; i++)
-	{
-		if (strcmp(emps_sim->ee[i].name, name) == 0)
-			pos=i;
-	}
-	if (pos == -1)
-	{
-		printf("找不到要删除有人员\n");
-	}
-	else
-	{
-		int j = 0;
-		for (j = pos; j < emps_sim->emp_sim_pre - 1; j++)
-		{
-			emps_sim->ee[j] = emps_sim->ee[j + 1];
-		}
-		emps_sim->emp_sim_pre--;
-		emps_sim->emp_sim_capability--;
-		FILE* pf = fopen("emp_sim.txt", "w");
-		if (pf == NULL)
-		{
-			perror("save error:");
-			return;
-		}
-		int i = 0;
-		for (i = 0; i < emps_sim->emp_sim_pre; i++)
-		{
-			fwrite(&(emps_sim->ee[i]), sizeof(emp_sim), 1, pf);
-		}
-		fclose(pf);
-		pf = NULL;
-		printf("删除成功\n");
-		FILE* pfsim = fopen("emp_sim.txt", "r");
-		i = 0;
-		printf("%-5s\t%-5s\n", "名字", "工资");
-		emp_sim tmp_sim;
-		for (i = 0; i < emps_sim->emp_sim_pre; i++)
-		{
-			fread(&tmp_sim, sizeof(emp_sim), 1, pfsim);
-			printf("%-5s\t%-5lf\n", tmp_sim.name, tmp_sim.salary);
-		}
-		free(emps_sim->ee);
-		emps_sim->ee = NULL;
-		fclose(pfsim);
-		pfsim = NULL;
-	}
-}
-void save(emp_info* emps)
-{
-	FILE* pf = fopen("employee.txt", "w");
-	if (pf == NULL)
-	{
-		perror("save error:");
-		return;
-	}
-	int i = 0;
-	for (i = 0; i < emps->emp_pre; i++)
-	{
-		fwrite(&(emps->e[i]), sizeof(emp), 1, pf);
-	}
-	fclose(pf);
-	pf = NULL;
-}
-void show(emp_info* emps)
-{
-	if (emps->emp_pre == 0)
-	{
-		printf("通讯录为空\n");
-	}
-	else
-	{
-		int i = 0;
-		printf("%-5s\t%-5s\t%-5s\n", "名字", "年龄", "工资");
-		for (i = 0; i < emps->emp_pre; i++)
-		{
-			printf("%s\t%d\t%lf\n", emps->e[i].name, emps->e[i].age, emps->e[i].salary);
-		}
-	}
-}
-void destory(emp_info* emps)
-{
-	free(emps->e);
-	emps->e = NULL;
-}
+//#define CAPABILITY 2
+//typedef struct employ
+//{
+//	char name[10];
+//	int age;
+//	double salary;
+//}emp;
+//typedef struct employ_sim
+//{
+//	char name[10];
+//	double salary;
+//}emp_sim;
+//typedef struct emp_info
+//{
+//	emp* e;
+//	int emp_pre;
+//	int emp_capability;
+//}emp_info;
+//typedef struct emp_sim_info
+//{
+//	emp_sim* ee;
+//	int emp_sim_pre;
+//	int emp_sim_capability;
+//}emp_sim_info;
+//enum option
+//{
+//	EXIT,
+//	ADD,
+//	DELETE,
+//	SAVE,
+//	SHOW,
+//	ADD_SIM,
+//	DELETE_SIM
+//};
+//void checkemp(emp_info* emps)
+//{
+//	emp* ptr;
+//	if (emps->emp_pre == emps->emp_capability)
+//	{
+//		ptr = (emp*)realloc(emps->e, (emps->emp_capability + 2) * sizeof(emp));
+//		if (ptr == NULL)
+//		{
+//			return;
+//		}
+//		emps->e = ptr;
+//		emps->emp_capability += 2;
+//		printf("增容成功\n");
+//	}
+//}
+//void loademp(emp_info* emps)
+//{
+//	emp tmp;
+//	FILE* pf = fopen("employee.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("load error:");
+//		return;
+//	}
+//	while (fread(&tmp, sizeof(emp), 1, pf))
+//	{
+//		checkemp(emps);
+//		emps->e[emps->emp_pre] = tmp;
+//		emps->emp_pre++;
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//}
+//void init(emp_info* emps)
+//{
+//	emps->e = (emp*)malloc(sizeof(emp) * 2);
+//	if (emps->e == NULL)
+//	{
+//		return;
+//	}
+//	emps->emp_pre = 0;
+//	emps->emp_capability = CAPABILITY;
+//	loademp(emps);
+//}
+//void add(emp_info* emps)
+//{
+//	checkemp(emps);
+//	printf("请输入名字:>\n");
+//	scanf("%s", emps->e[emps->emp_pre].name);
+//	printf("请输入年龄:>\n");
+//	scanf("%d", &(emps->e[emps->emp_pre].age));
+//	printf("请输入工资:>\n");
+//	scanf("%lf", &(emps->e[emps->emp_pre].salary));
+//	emps->emp_pre++;
+//	printf("添加成功\n");
+//}
+//void add_sim(emp_info* emps, emp_sim_info* emps_sim)
+//{
+//	emp tmp;
+//	emp_sim tmp_sim;
+//	FILE* pf = fopen("employee.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("load employee error:");
+//		return;
+//	}
+//	FILE* pf_sim = fopen("emp_sim.txt", "w");
+//	if (pf_sim == NULL)
+//	{
+//		perror("load emp_sim error:");
+//		return;
+//	}
+//	while (fread(&tmp, sizeof(emp), 1, pf))
+//	{
+//		strcpy(tmp_sim.name, tmp.name);
+//		tmp_sim.salary = tmp.salary; 
+//		fwrite(&tmp_sim, sizeof(emp_sim), 1, pf_sim);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	fclose(pf_sim);
+//	pf_sim = NULL;
+//	printf("已建立简明职工工资文件\n");
+//	FILE* pfsim = fopen("emp_sim.txt", "r");
+//	int i = 0;
+//	printf("%-5s\t%-5s\n", "名字", "工资");
+//	for (i = 0; i < emps->emp_pre; i++)
+//	{
+//		fread(&tmp_sim, sizeof(emp_sim), 1, pfsim);
+//		printf("%-5s\t%-5lf\n", tmp_sim.name, tmp_sim.salary);
+//	}
+//	fclose(pfsim);
+//	pfsim = NULL;
+//	emps_sim->emp_sim_pre = emps->emp_pre;
+//	emps_sim->emp_sim_capability = emps->emp_capability;
+//}
+//int findemp(emp_info* emps,char* name)
+//{
+//	int i = 0;
+//	for (i = 0; i < emps->emp_pre; i++)
+//	{
+//		if (strcmp(emps->e[i].name, name) == 0)
+//		{
+//			return i;
+//		}
+//	}
+//	return -1;
+//}
+//void delete(emp_info* emps)
+//{
+//	char name[10];
+//	printf("请输入要删除人员的名字：");
+//	scanf("%s", name);
+//	int pos = findemp(emps, name);
+//	if (pos == -1)
+//	{
+//		printf("找不到要删除有人员\n");
+//	}
+//	else
+//	{
+//		int j = 0;
+//		for (j = pos; j < emps->emp_pre - 1; j++)
+//		{
+//			emps->e[j] = emps->e[j + 1];
+//		}
+//		emps->emp_pre--;
+//		printf("删除成功\n");
+//	}
+//}
+//void delete_sim(emp_sim_info* emps_sim)
+//{
+//	char name[10];
+//	printf("请输入要删除人员(emp_sim)的名字：");
+//	scanf("%s", name);
+//	emp_sim tmp;
+//	FILE* pf1 = fopen("emp_sim.txt", "r");
+//	if (pf1 == NULL)
+//	{
+//		perror("load error:");
+//		return;
+//	}
+//	emps_sim->ee = (emp_sim*)malloc(sizeof(emp_sim) * 20);
+//	if (emps_sim->ee == NULL)
+//	{
+//		perror("load error:");
+//		return;
+//	}
+//	int i = 0;
+//	while (fread(&tmp, sizeof(emp_sim), 1, pf1))
+//	{
+//		emps_sim->ee[i++] = tmp;
+//	}
+//	fclose(pf1);
+//	pf1 = NULL;
+//	emps_sim->emp_sim_pre=i;
+//	i = 0;
+//	int pos = -1;
+//	for (i = 0; i < emps_sim->emp_sim_pre; i++)
+//	{
+//		if (strcmp(emps_sim->ee[i].name, name) == 0)
+//			pos=i;
+//	}
+//	if (pos == -1)
+//	{
+//		printf("找不到要删除有人员\n");
+//	}
+//	else
+//	{
+//		int j = 0;
+//		for (j = pos; j < emps_sim->emp_sim_pre - 1; j++)
+//		{
+//			emps_sim->ee[j] = emps_sim->ee[j + 1];
+//		}
+//		emps_sim->emp_sim_pre--;
+//		emps_sim->emp_sim_capability--;
+//		FILE* pf = fopen("emp_sim.txt", "w");
+//		if (pf == NULL)
+//		{
+//			perror("save error:");
+//			return;
+//		}
+//		int i = 0;
+//		for (i = 0; i < emps_sim->emp_sim_pre; i++)
+//		{
+//			fwrite(&(emps_sim->ee[i]), sizeof(emp_sim), 1, pf);
+//		}
+//		fclose(pf);
+//		pf = NULL;
+//		printf("删除成功\n");
+//		FILE* pfsim = fopen("emp_sim.txt", "r");
+//		i = 0;
+//		printf("%-5s\t%-5s\n", "名字", "工资");
+//		emp_sim tmp_sim;
+//		for (i = 0; i < emps_sim->emp_sim_pre; i++)
+//		{
+//			fread(&tmp_sim, sizeof(emp_sim), 1, pfsim);
+//			printf("%-5s\t%-5lf\n", tmp_sim.name, tmp_sim.salary);
+//		}
+//		free(emps_sim->ee);
+//		emps_sim->ee = NULL;
+//		fclose(pfsim);
+//		pfsim = NULL;
+//	}
+//}
+//void save(emp_info* emps)
+//{
+//	FILE* pf = fopen("employee.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("save error:");
+//		return;
+//	}
+//	int i = 0;
+//	for (i = 0; i < emps->emp_pre; i++)
+//	{
+//		fwrite(&(emps->e[i]), sizeof(emp), 1, pf);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//}
+//void show(emp_info* emps)
+//{
+//	if (emps->emp_pre == 0)
+//	{
+//		printf("通讯录为空\n");
+//	}
+//	else
+//	{
+//		int i = 0;
+//		printf("%-5s\t%-5s\t%-5s\n", "名字", "年龄", "工资");
+//		for (i = 0; i < emps->emp_pre; i++)
+//		{
+//			printf("%s\t%d\t%lf\n", emps->e[i].name, emps->e[i].age, emps->e[i].salary);
+//		}
+//	}
+//}
+//void destory(emp_info* emps)
+//{
+//	free(emps->e);
+//	emps->e = NULL;
+//}
+//int main()
+//{
+//	printf("**************************\n");
+//	printf("*** 1.add     2.delete ***\n");
+//	printf("*** 3.save    4.show   ***\n");
+//	printf("*** 5.add_sim 6.delete_sim\n");
+//	printf("*** 0.exit             ***\n");
+//	printf("**************************\n");
+//	emp_info emps;
+//	emp_sim_info emps_sim;
+//	init(&emps);
+//	int input = 0;
+//	init(&emps);
+//	do
+//	{
+//		printf("input your choice:");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case ADD:
+//			add(&emps);
+//			break;
+//		case DELETE:
+//			delete(&emps);
+//			break;
+//		case SAVE:
+//			save(&emps);
+//			break;
+//		case SHOW:
+//			show(&emps);
+//			break;
+//		case ADD_SIM:
+//			add_sim(&emps, &emps_sim);
+//			break;
+//		case DELETE_SIM:
+//		    delete_sim(&emps_sim);
+//		    break;
+//		case EXIT:
+//			printf("退出\n");
+//			save(&emps);
+//			destory(&emps);
+//			break;
+//		default:
+//			printf("input error\n");
+//		}
+//	} while (input);
+//	return 0;
+//}
 int main()
 {
-	printf("**************************\n");
-	printf("*** 1.add     2.delete ***\n");
-	printf("*** 3.save    4.show   ***\n");
-	printf("*** 5.add_sim 6.delete_sim\n");
-	printf("*** 0.exit             ***\n");
-	printf("**************************\n");
-	emp_info emps;
-	emp_sim_info emps_sim;
-	init(&emps);
-	int input = 0;
-	init(&emps);
-	do
+	int len = 0;
+	char c='\0';
+	int size = 1;
+	char* ptr = (char*)malloc(size);
+	char* tmp;
+	FILE* pf = fopen("alpha.txt", "w");
+	printf("请输入字符串:\n");
+	while (*(ptr + len) != '\0')
 	{
-		printf("input your choice:");
-		scanf("%d", &input);
-		switch (input)
+		len = 0;
+		while (c = getchar() != '\n')
 		{
-		case ADD:
-			add(&emps);
-			break;
-		case DELETE:
-			delete(&emps);
-			break;
-		case SAVE:
-			save(&emps);
-			break;
-		case SHOW:
-			show(&emps);
-			break;
-		case ADD_SIM:
-			add_sim(&emps, &emps_sim);
-			break;
-		case DELETE_SIM:
-		    delete_sim(&emps_sim);
-		    break;
-		case EXIT:
-			printf("退出\n");
-			save(&emps);
-			destory(&emps);
-			break;
-		default:
-			printf("input error\n");
+			if (len >= size)
+			{
+				size *= 2;
+				tmp = (char*)realloc(ptr, size);
+				if (tmp == NULL)
+				{
+					perror("tmp error:");
+					return 0;
+				}
+				ptr = tmp;
+			}
+			*(ptr + len) = c;
+			fwrite((ptr + len), sizeof(char), 1, pf);
+			len++;
 		}
-	} while (input);
+	}
+	fwrite("\0", sizeof(char), 1, pf);
+	free(ptr);
+	ptr = NULL;
+	fclose(pf);
+	pf = NULL;
 	return 0;
 }
